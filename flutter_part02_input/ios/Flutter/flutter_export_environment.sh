bd2ec62f4400c5d08de0056b18c599f5b14c1e38
javadori <javadori@gmail.com>
2024-01-27 19:59:20 +0900

    입력방식 예제

---
+++ flutter_part02_input/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/javadori/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/javadori/StudioProjects/flutter_part02_input"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/javadori/StudioProjects/flutter_part02_input/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC83YTUwMjIxNzMzYzI4YTU2MDJiMWZkN2ZiZjAxNzAzMzlhOGM0YmQwLw=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/javadori/StudioProjects/flutter_part02_input/.dart_tool/package_config.json"
