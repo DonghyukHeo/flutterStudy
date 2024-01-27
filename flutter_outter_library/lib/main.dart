e1bbb1eb5af5432f93f5519b5ac244a9ada9c28e
javadori <javadori@gmail.com>
2024-01-27 20:52:32 +0900

    외부라이브러리 사용 예제

---
+++ flutter_outter_library/lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text('외부 라이브러리 사용하기'),
+        ),
+        body: const Body(),
+      ),
+    );
+  }
+}
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return RandomWords();
+  }
+}
+
+class RandomWords extends StatelessWidget {
+  const RandomWords({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final wordList = generateWordPairs().take(5).toList();
+    final widgets = wordList
+        .map(
+          (word) => Text(
+            word.asCamelCase,
+            style: const TextStyle(fontSize: 32),
+          ),
+        )
+        .toList();
+
+    return Column(
+      children: widgets,
+    );
+  }
+}
